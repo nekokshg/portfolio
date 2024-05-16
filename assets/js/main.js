d11d9933fd191caba578fe70ef4e1c3f45fc957c
nicole <nicolekaneshige@gmail.com>
2024-05-16 13:41:53 -0700

    code email sending through contact form in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,7 +39,33 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_fhrcwji','template_q4pb7jk','#contact-form','jFCNq62BjmY5KO_Hw')
+    .then(() =>{
+        //Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+        
+        //Remove message after five seconds
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        //Clear input fields
+        contactForm.reset()
+    
+    }, () => {
+        //Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
