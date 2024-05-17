d7bcc5065d3238af6e3bc5b9c83c57a0076bba52
nicole <nicolekaneshige@gmail.com>
2024-05-17 13:01:31 -0700

    code show scroll up in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,7 +68,14 @@ const sendEmail = (e) =>{
 contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    //when the scroll is higher than 350 viewport height, add the show scroll
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
 
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
